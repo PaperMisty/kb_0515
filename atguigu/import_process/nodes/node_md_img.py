2892318a97d22fd7bf7aa0dc48afa76fa309f2f8
PaperMisty <2061478506@qq.com>
2026-08-03 18:29:22 +0800

    start_time

--- atguigu/import_process/nodes/node_md_img.py
+++ atguigu/import_process/nodes/node_md_img.py
@@ -8,6 +8,8 @@ from atguigu.config.config import RAW_DIR, OUTPUT_DIR
 from pathlib import Path
 import os, re, time
 from rich import print
+from langchain.chat_models import init_chat_model
+from atguigu.config.config import LLMConfig
 
 
 class NodeMDImg(NodeBase):
@@ -96,10 +98,7 @@ class NodeMDImg(NodeBase):
         return img_context_list, img_name, img_path
 
     def get_img_abstract(self, img_context_list, img_name, img_path):
-        # 创建模型
-        from langchain.chat_models import init_chat_model
-        from atguigu.config.config import LLMConfig
-
+        # 初始化VLM模型
         llm = init_chat_model(
             model=LLMConfig.vlm_model,
             model_provider="openai",
@@ -107,21 +106,24 @@ class NodeMDImg(NodeBase):
             api_key=LLMConfig.api_key,
             temperature=LLMConfig.temperature,
         )
-        img_summary_list = []
+
         # 设计令牌桶,防止限流-----bug: 可能不能打满请求RPM的80% ,而且尚未考虑TPM限制
         bucket = deque(maxlen=30)
         current_time = time.time()
         for img_context in img_context_list:
+            start_time = time.time()
             # 盲清一波队列
             while bucket and current_time - bucket[0] > 60:
                 bucket.popleft()
-            # 如果满员, 睡一定时间,睡完再入队新的请求时间戳
+            # 如果满员, 睡一定时间,睡完再出队第一个, 并入队新的请求时间戳(通过睡眠来控制后文的请求频率)
             if bucket and len(bucket) == deque.maxlen:
                 time.sleep(60 - (current_time - bucket[0]))
                 current_time = time.time()
                 while bucket and current_time - bucket[0] > 60:
                     bucket.popleft()
+            # 最开始, bucket为空队列, 可以快速打满30个请求, 并且append进去;(归根结底还是串行,而且不方便开协程优化)
             bucket.append(current_time)
+
             # 图片转Base64
             import base64
 
@@ -141,17 +143,18 @@ class NodeMDImg(NodeBase):
                         },
                         {
                             "type": "text",
-                            "text": f"""这是一张图片，图片上文部分为"{img_context.get("pre_text")}"，
-                                下文部分为"{img_context.get("post_text")}"，请用中文简要总结这张图片的摘要,字数在50字以内。""",
+                            "text": f"""这是一张图片，图片上文部分为"{img_context.get("pre_context")}"，
+                                下文部分为"{img_context.get("post_context")}"，请用中文简要总结这张图片的摘要,字数在50字以内。""",
                         },
                     ],
                 },
             ]
-            res = llm.invoke(input=messages)
-            img_summary_list.append(
-                {"img_name": img_name, "img_path": img_path, "summary": res.content}
-            )
-        logger.info(img_summary_list)
+            res = llm.invoke(
+                input=messages
+            ).content  # 假设模型处理一张图1s, 那么队列中的间隔都为1s, 实际RPM=1 request / s
+            print("用时: ", time.time() - start_time, "s:", res)
+            img_context["img_summary"] = res
+        logger.info(f"{img_path}内部的图片摘要获取完毕")
 
 
 if __name__ == "__main__":
