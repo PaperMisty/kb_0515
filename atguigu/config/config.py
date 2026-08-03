1e38c938b9800c3d4547b80c7151b0a4f4eda45e
PaperMisty <2061478506@qq.com>
2026-08-03 16:54:11 +0800

    feat:提取md里面的图片,获取上下文

--- atguigu/config/config.py
+++ atguigu/config/config.py
@@ -2,7 +2,10 @@ from dotenv import load_dotenv
 import os
 from pathlib import Path
 
-load_dotenv()
+load_dotenv(
+    override=True
+)  # override=True强制用 .env 文件中的值覆盖掉系统环境变量的值。
+# load_dotenv 从当前目录开始，递归向上查找.env文件。最先找到的那个.env文件会被加载。
 
 ROOT_DIR = Path(__file__).parents[2]
 DATA_DIR = ROOT_DIR / "atguigu" / "data"
@@ -13,3 +16,12 @@ OUTPUT_DIR = DATA_DIR / "output"
 class MineruConfig:
     mineru_token = os.getenv("mineru_token")
     mineru_base_url = os.getenv("mineru_base_url")
+
+
+class LLMConfig:
+    api_key = os.getenv("OPENAI_API_KEY")
+    base_url = os.getenv("OPENAI_API_BASE")
+    default_model = os.getenv("LLM_DEFAULT_MODEL")
+    temperature = float(os.getenv("LLM_DEFAULT_TEMPERATURE"))
+    vlm_model = os.getenv("VL_MODEL")
+    item_model = os.getenv("ITEM_MODEL")
--- atguigu/import_process/nodes/node_md_img.py
+++ atguigu/import_process/nodes/node_md_img.py
@@ -1,11 +1,13 @@
 # atguigu/import_process/nodes/node_md_img.py
+from collections import deque
 from atguigu.tool.json_format_tool import json_format
 from atguigu.tool.logger import logger
 from atguigu.import_process.base import NodeBase
 from atguigu.import_process.state import ImportGraphState
 from atguigu.config.config import RAW_DIR, OUTPUT_DIR
 from pathlib import Path
-import os, re
+import os, re, time
+from rich import print
 
 
 class NodeMDImg(NodeBase):
@@ -15,26 +17,51 @@ class NodeMDImg(NodeBase):
 
     name = "node_md_img"
 
+    # 路径文件检查器
+    @staticmethod
+    def validate_path(path: str, level: str):
+        # 判定路径是否可得
+        if not path or (not path.exists()):
+            if level == "error":
+                logger.error(f"路径无法读取/无内容:{path=}")
+                raise FileNotFoundError("路径无法读取")
+            elif level == "warning":
+                logger.warning(f"路径无法读取/无内容:{path=}")
+            elif level == "info":
+                logger.info(f"路径无法读取/无内容:{path=}")
+            elif level == "debug":
+                logger.debug(f"路径无法读取/无内容:{path=}")
+        return path
+
     def process(self, state: ImportGraphState):
-        md_path = state.get("md_path")
-        # 判定文件是否可得
-        if not md_path or (not md_path.exists()):
-            logger.error(f"md路径无法读取:{md_path=}")
-            raise FileNotFoundError("md路径无法读取")
+        # 1.获取图片
+        md_path_img_list, content, md_path_img = self.get_img(state)
+        # 2.获取图片上下文
+        img_context_list, img_name, img_path = self.get_img_context(
+            md_path_img_list, content, md_path_img
+        )
+        # 3.获取图片摘要
+        img_summary_list = self.get_img_abstract(img_context_list, img_name, img_path)
+        return state
+
+    def get_img(self, state: ImportGraphState):
+        # 判定文件路径存在性
+        md_path = self.validate_path(state.get("md_path"), level="error")
         with open(md_path, "r", encoding="utf-8") as f:
             content = f.read()
         # 判定文件内容存在性
         if not content:
             logger.error("文件没有内容")
             raise ValueError("文件没有内容")
-        md_path_img = md_path.parent / "images"
-        # 判定图片存在性
-        if not md_path_img or (not md_path_img.exists()):
-            logger.warning(f"md_img路径无法读取:{md_path_img=}")
+        # 判定图片文件夹存在性
+        md_path_img = self.validate_path(md_path.parent / "images", level="warning")
+
         md_path_img_list = os.listdir(md_path_img)
         if not md_path_img_list:
-            logger.info(f"md_path_img_list无内容")
+            logger.info(f"md_path文件夹下面无内容")
+        return md_path_img_list, content, md_path_img
 
+    def get_img_context(self, md_path_img_list, content, md_path_img):
         # 2.遍历图片,获取上下文
         IMG_SUFFIX_SET = {".jpg", ".png", ".jpeg", ".gif", ".webp", ".bmp"}
         MAX_CONTEXT = 250
@@ -61,11 +88,70 @@ class NodeMDImg(NodeBase):
                     "img_name": img_name,
                     "pre_context": pre_context,
                     "post_content": post_context,
-                    "img_path": str(md_path_img / img_name),
+                    "img_path": (img_path := str(md_path_img / img_name)),
                 }
             )
-            print(json_format(img_context_list))
-        return state
+            # print(json_format(img_context_list))
+        logger.info(f"{md_path_img}内部的图片上下文获取完毕")
+        return img_context_list, img_name, img_path
+
+    def get_img_abstract(self, img_context_list, img_name, img_path):
+        # 创建模型
+        from langchain.chat_models import init_chat_model
+        from atguigu.config.config import LLMConfig
+
+        llm = init_chat_model(
+            model=LLMConfig.vlm_model,
+            model_provider="openai",
+            base_url=LLMConfig.base_url,
+            api_key=LLMConfig.api_key,
+            temperature=LLMConfig.temperature,
+        )
+        img_summary_list = []
+        # 设计令牌桶,防止限流-----bug: 可能不能打满请求RPM的80% ,而且尚未考虑TPM限制
+        bucket = deque(maxlen=30)
+        current_time = time.time()
+        for img_context in img_context_list:
+            # 盲清一波队列
+            while bucket and current_time - bucket[0] > 60:
+                bucket.popleft()
+            # 如果满员, 睡一定时间,睡完再入队新的请求时间戳
+            if bucket and len(bucket) == deque.maxlen:
+                time.sleep(60 - (current_time - bucket[0]))
+                current_time = time.time()
+                while bucket and current_time - bucket[0] > 60:
+                    bucket.popleft()
+            bucket.append(current_time)
+            # 图片转Base64
+            import base64
+
+            with open(img_context.get("img_path"), "rb") as f:
+                b_content = f.read()
+                base64_code = base64.b64encode(b_content).decode("utf-8")
+            # 构造提示词
+            messages = [
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64_code}",
+                            },
+                        },
+                        {
+                            "type": "text",
+                            "text": f"""这是一张图片，图片上文部分为"{img_context.get("pre_text")}"，
+                                下文部分为"{img_context.get("post_text")}"，请用中文简要总结这张图片的摘要,字数在50字以内。""",
+                        },
+                    ],
+                },
+            ]
+            res = llm.invoke(input=messages)
+            img_summary_list.append(
+                {"img_name": img_name, "img_path": img_path, "summary": res.content}
+            )
+        logger.info(img_summary_list)
 
 
 if __name__ == "__main__":
