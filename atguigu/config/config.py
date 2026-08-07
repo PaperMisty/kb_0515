82c9c0b0596e45ad6cf7d22c82f3b5c4cbd3def0
PaperMisty <2061478506@qq.com>
2026-08-07 11:39:04 +0800

    chores :config配置

--- atguigu/config/config.py
+++ atguigu/config/config.py
@@ -34,3 +34,12 @@ class MinIOConfig:
     minio_secret = os.getenv("MINIO_SECRET")
     minio_bucket_name = os.getenv("MINIO_BUCKET_NAME")
     minio_img_dir = os.getenv("MINIO_IMG_DIR")
+
+
+# config/config.py
+class EmbeddingConfig:
+    bge_m3_path = (os.getenv("BGE_M3_PATH"),)
+    bge_m3 = (os.getenv("BGE_M3"),)
+    bge_device = (os.getenv("BGE_DEVICE"),)
+    # 特殊处理：将.env中的1/0转为布尔值，兼容常见的数字/字符串格式
+    bge_fp16 = (os.getenv("BGE_FP16") in ("1", "True", "true", 1),)
