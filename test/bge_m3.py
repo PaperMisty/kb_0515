5f9e392b3492b78425cd38d1bad17380b8c1ebd5
PaperMisty <2061478506@qq.com>
2026-08-07 12:05:28 +0800

    chores:

--- test/bge_m3.py
+++ test/bge_m3.py
@@ -1,5 +1,5 @@
 # test/bge_m3测试向量转换.py
-from atguigui.import_process.utils.embedding_utils import get_embeddings
+from atguigui.utils.embedding_utils import get_embeddings
 
 result = get_embeddings(["测试", "test"])
 
